dbbf57cda74da4c8a1ad658bbee64309a77748f9
ashish-m-jacob <ashishmjacob@gmail.com>
2024-10-18 00:43:30 +0530

    Updated with error handling and search feature for stored passwords

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_pass():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -32,18 +33,49 @@ def submit_details():
     email = uname_entry.get()
     password = pass_entry.get()
 
+    new_data = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
 
     else:
-        is_okay = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}\nPassword: {password}\nIs it okay to save?")
+        try:
+            with open("info.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
 
-        if is_okay:
-            with open("info.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
+        except FileNotFoundError:
+            with open("info.json", "w") as file:
+                json.dump(new_data, file, indent=4)
 
-                website_entry.delete(0, END)
-                pass_entry.delete(0, END)
+        else:
+            with open("info.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
+
+# -------------------------- SEARCH PASSWORD -------------------------- #
+def search_pass():
+    website_name = website_entry.get()
+
+    try:
+        with open("info.json", "r") as file:
+            data = json.load(file)
+            email = data[website_name]["email"]
+            password = data[website_name]["password"]
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="You have not populated the manager with information yet. Please add a few accounts")
+    except KeyError:
+        messagebox.showinfo(title="Not found", message="The website you were looking for is not in this database. Please try a different one")
+    else:
+        messagebox.showinfo(title="Credentials", message=f"Here are the details you requested!\n"
+                                                         f"Email: {email}\nPassword: {password}")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -68,7 +100,7 @@ pass_label.config(text="Password:", justify="right")
 pass_label.grid(row=3, column=0, sticky="w")
 
 # Entries
-website_entry = Entry(width=42)
+website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
 website_entry.focus()
 
@@ -76,14 +108,17 @@ uname_entry = Entry(width=42)
 uname_entry.grid(row=2, column=1, columnspan=2, sticky="w")
 uname_entry.insert(0, "example@gmail.com")
 
-pass_entry = Entry(width=21, justify="left")
+pass_entry = Entry(width=35, justify="left")
 pass_entry.grid(row=3, column=1, columnspan=1, sticky="w")
 
 # Buttons
-gen_pass = Button(text="Generate Password", justify="left", command=gen_pass)
-gen_pass.grid(row=3, column=2)
+gen_pass = Button(text="Generate Password", justify="left", width=14, command=gen_pass)
+gen_pass.grid(row=3, column=2, sticky="e")
 
 add_button = Button(text="Add", width=59, command=submit_details)
 add_button.grid(row=4, column=0, columnspan=3, sticky="e")
+
+search_button = Button(text="Search", command=search_pass, width=14)
+search_button.grid(row=1, column=2, sticky="e")
 
 window.mainloop()
